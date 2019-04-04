09699f45c1eaaaceb9f4ff76df115aac616cd9ec
Linaibrahim <lina.ibrahim.mohamed@gmail.com>
2019-04-04 10:00:09 +0200

    Add files via upload

---
+++ BasisClass.java
@@ -0,0 +1,27 @@
+package pharmacy2;
+
+public class BasisClass {
+    
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sunitha
+ */
+
+public static int manu_id,itemcount,S_no,customerid,customer_purchases;
+public static  StringBuffer id=new StringBuffer();
+public static  StringBuffer pwd=new StringBuffer();
+public static String med_code;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
